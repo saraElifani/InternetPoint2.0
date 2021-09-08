8b48b3ebc8868ff758a5df3a863cf0e542ca0a80
saradeveloper92 <sara.elifani92@gmail.com>
2021-09-08 11:08:17 +0200

    gestione errori ed eccezioni con try catch

--- src/java/Main.java
+++ src/java/Main.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 public class Main {
     public static Cliente[] clienti = new Cliente[10];
     public static int nClienti = 0;
+    static boolean programContinue = false;
 
     public static void main(String[] args) {
 
-          while (true){
-              menu();
-          }
+        do{
+            menu();
+        }while (programContinue);
     }
 
     public static void menu() {
@@ -18,27 +19,49 @@ public class Main {
                 "Menu", JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
         System.out.println(menu);
         if (menu == null) {
-            System.exit(0);
+            menu = "Hai annullato";
+            programContinue= false;
         }
 
         switch (menu) {
             case "1. Visualizza Lista Clienti":
                 visualizzaListaClienti();
+                programContinue= true;
                 break;
             case "2. Inserisci Cliente":
-                inserisciNuovoCliente();
+                try{
+                    inserisciNuovoCliente();
+                }catch (NullPointerException e){
+
+                }
+                programContinue= true;
                 break;
             case "3. Modifica Cliente":
-                modificaCliente();
+                try {
+                    modificaCliente();
+                }catch (NullPointerException e){
+
+                }
+                programContinue= true;
                 break;
             case "4. Cancella Cliente":
-                cancellaCliente();
+                try {
+                    cancellaCliente();
+                }catch (NullPointerException e){
+
+                }
+                programContinue= true;
                 break;
             case "5. Checkout":
-                eseguiCheckout();
+                try {
+                    eseguiCheckout();
+                }catch (NullPointerException e){
+
+                }
+                programContinue= true;
                 break;
             case "6. Esci":
-                System.exit(0);
+                programContinue= false;
                 break;
         }
     }
@@ -49,45 +72,45 @@ public class Main {
         JOptionPane.showMessageDialog(null, builder.toString(), "Lista clienti", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    public static void inserisciNuovoCliente() {
+    public static void inserisciNuovoCliente() throws NullPointerException {
         String nome = JOptionPane.showInputDialog(null, "inserisci nome");
-        if (nome == null) menu();
+        //if (nome == null) return;
         String cognome = JOptionPane.showInputDialog(null, "inserisci cognome");
-        if (cognome == null) menu();
+        //if (cognome == null) return;
         String oraInizio = JOptionPane.showInputDialog(null, "Inserisci l'orario di inizio in formato HH:MM");
-        if (oraInizio == null) menu();
+        //if (oraInizio == null) return;
         Cliente cliente = new Cliente(nome, cognome, oraInizio);
         clienti[nClienti] = cliente;
         nClienti++;
 
     }
 
-    public static void modificaCliente() {
+    public static void modificaCliente() throws NullPointerException {
         visualizzaListaClienti();
         String indice = JOptionPane.showInputDialog(null, "Inserisci l'indice del cliente da modificare", "", JOptionPane.INFORMATION_MESSAGE);
-        if (indice == null) menu();
+       // if (indice == null) return;
         String nome = JOptionPane.showInputDialog(null, "inserisci nome");
-        if (nome == null) menu();
+       // if (nome == null) return;
         clienti[Integer.parseInt(indice)].setNome(nome);
         String cognome = JOptionPane.showInputDialog(null, "inserisci cognome");
-        if (cognome == null) menu();
+        //if (cognome == null) return;
         clienti[Integer.parseInt(indice)].setCognome(cognome);
     }
 
-    public static void cancellaCliente() {
+    public static void cancellaCliente() throws NullPointerException {
         visualizzaListaClienti();
         String index = JOptionPane.showInputDialog(null, "Inserisci l'indice del cliente da cancellare", "", JOptionPane.QUESTION_MESSAGE);
-        if (index == null) menu();
+        //if (index == null) return;
         clienti[Integer.parseInt(index)] = null;
     }
 
-    public static void eseguiCheckout() {
+    public static void eseguiCheckout() throws NullPointerException {
         visualizzaListaClienti();
         String index = JOptionPane.showInputDialog(null, "Inserisci l'indice del cliente", "", JOptionPane.QUESTION_MESSAGE);
-        if (index == null) menu();
+       // if (index == null) return;
         String orarioFine = JOptionPane.showInputDialog(null, "Inserisci l'orario di fine in formato HH:MM",
                 "orario di fine", JOptionPane.QUESTION_MESSAGE);
-        if (orarioFine == null) menu();
+        //if (orarioFine == null) return;
         clienti[Integer.parseInt(index)].setOraFine(orarioFine);
         String oraInizioDaSplittare = clienti[Integer.parseInt(index)].getOraInizio();
         String[] parts = oraInizioDaSplittare.split(":");
@@ -114,5 +137,4 @@ public class Main {
         double tariffa = (ore + (mezzOra * 0.5) + (quartiDora * 0.3));
         JOptionPane.showMessageDialog(null, "Importo: " + tariffa, "Checkout", JOptionPane.INFORMATION_MESSAGE);
     }
-
 }
